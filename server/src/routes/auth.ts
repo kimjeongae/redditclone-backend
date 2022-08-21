9e91a659d49557760f85191f77aeefad9c81837b
kimjeongae <kjaa34@gmail.com>
2022-08-22 00:51:26 +0900

    로그인 처리&유저정보 context에 담기

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,9 @@
-import { validate } from "class-validator";
+import { isEmpty, validate } from "class-validator";
 import { Request, Response, Router } from "express";
 import User from "../entities/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 const mapError = (errors: Object[]) => {
     return errors.reduce((prev: any, err: any) => {
@@ -42,7 +45,51 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
+const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+      let errors: any = {};
+
+      if (isEmpty(username))
+        errors.username = "사용자 이름은 비워둘 수 없습니다.";
+      if (isEmpty(password)) errors.password = "비밀번호는 비워둘 수 없습니다.";
+      if (Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+      }
+
+      const user = await User.findOneBy({ username });
+  
+      if (!user)
+        return res
+          .status(404)
+          .json({ username: "사용자 이름이 등록되지 않았습니다." });
+  
+      const passwordMatches = await bcrypt.compare(password, user.password);
+  
+      if (!passwordMatches) {
+        return res.status(401).json({ password: "비밀번호가 잘못되었습니다." });
+      }
+  
+      const token = jwt.sign({ username }, process.env.JWT_SECRET);
+  
+      res.set(
+        "Set-Cookie",
+        cookie.serialize("token", token, {
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7,
+          path: "/",
+        })
+      );
+  
+      return res.json({ user, token });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  };
+
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
