bf48354ade2787ec9ff42ee95b27dad2fce3c812
kimjeongae <kjaa34@gmail.com>
2022-08-24 11:19:30 +0900

    로그아웃 기능 처리

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -75,12 +75,12 @@ const login = async (req: Request, res: Response) => {
         const token = jwt.sign({ username }, process.env.JWT_SECRET);
 
         res.set(
-        "Set-Cookie",
-        cookie.serialize("token", token, {
-            httpOnly: true,
-            maxAge: 60 * 60 * 24 * 7,
-            path: "/",
-        })
+            "Set-Cookie",
+            cookie.serialize("token", token, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 24 * 7,
+                path: "/",
+            })
         );
 
         return res.json({ user, token });
@@ -94,9 +94,26 @@ const me = async (_: Request, res: Response) => {
     return res.json(res.locals.user);
 };
 
+const logout = async (_:Request, res: Response) => {
+    res.set(
+        "Set-Cookie",
+        cookie.serialize("token", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            expires: new Date(0),
+            path: "/",
+        })
+    );
+    
+    res.status(200).json({success: true});
+};
+
+
 const router = Router();
 router.get("/me", userMiddleware, authMiddleware, me);
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", userMiddleware, authMiddleware, logout);
 
 export default router;
