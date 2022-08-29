872c1a5a6fc4d4367de20b61f9c958d4b3f1efe6
kimjeongae <kjaa34@gmail.com>
2022-08-30 03:44:39 +0900

    changed

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-            name:"reddit-client",
+            name: "reddit-client",
             script: "npm run start:prod"
         }]
 }
--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     domains: ["www.gravatar.com", 
               "localhost", 
-              "ec2-18-218-166-207.us-east-2.compute.amazonaws.com"
+              "ec2-18-225-31-96.us-east-2.compute.amazonaws.com"
             ]
   }
 }
